86c7aa1917f98b8f6ce0516c50281b5de3243a31
Gilvanov Dmitry <lucefer13+github@gmail.com>
2020-06-11 15:24:01 +0500

    1 5 logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -6,7 +6,13 @@
  * GilvanovDR (c) 2020.
  */
 
+/*
+ * GilvanovDR (c) 2020.
+ */
+
 package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,10 +20,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class UserServlet extends HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to users");
+
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
     }
