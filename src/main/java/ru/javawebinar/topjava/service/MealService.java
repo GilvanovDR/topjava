b3e149c790035818656dbf446d742ef7328fd896
Gilvanov Dmitry <lucefer13+github@gmail.com>
2020-06-20 10:27:30 +0500

    HW2: 4- 5 Add MealService Add MealRestController Add MealService, MealRestController, InMemoryMealRepository to Spring Context

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,40 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
+
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal meal, Integer authUserID) {
+        return repository.save(meal, authUserID);
+    }
+
+    public void delete(int id, Integer authUserID) {
+        checkNotFoundWithId(repository.delete(id, authUserID), id);
+    }
+
+    public Meal get(int id, Integer authUserID) {
+        return checkNotFoundWithId(repository.get(id, authUserID), id);
+    }
+
+    public Collection<Meal> getAll(Integer authUserID) {
+        return repository.getAll(authUserID);
+    }
+
+    public void update(Meal meal, Integer authUserID) {
+        checkNotFoundWithId(repository.save(meal, authUserID), meal.getId());
+    }
 
 }
